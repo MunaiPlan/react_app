2176f618485813052e896383a1c13e87e2edb0d2
KadirbekSharau <sharaukadr2001@gmail.com>
2023-06-20 13:40:33 +0600

    example

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
-import React from "react";
+//import React from "react";
 
-export default function Header(){
-    return (
-        <h1>Hello World</h1>
-    )
-}
+// export default function Hhleader(){
+//     return (
+//         <h1>Hello World</h1>
+//     )
+// }
